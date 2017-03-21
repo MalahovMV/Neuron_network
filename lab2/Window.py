e051aaca342d4ad34435c7bfd596f39fcc692b2e
malahov <misha.malaxow@yandex.ru>
2017-03-21 13:11:59 +0300

    Lab2

--- lab2/Window.py
+++ lab2/Window.py
@@ -1,24 +1,48 @@
 class Window:
 
-    def __init__(self, rate_learning, input_matrix):
-        self.y_true = []
+    def __init__(self, rate_learning, y_true, size_window):
+        self.y_true = y_true
         self.rate_learning = rate_learning
         self.epochs = 0
-        self.input_matrix = input_matrix
-        self.weight = [0, 0, 0, 0, 0]
+        self.weight = [0, 0, 0, 0]
         self.error_on_last_epochs = 0
+        self.size_window = size_window
+        self.vector_ans_on_epoch = []
 
     def during_epochs(self):
-        y_calculate_vector = []
-        for i in range(len(self.input_matrix)):
+        for i in range(self.size_window, len(self.y_true) - 1):
             y_calculate = 0
-            for j in range(len(self.input_matrix[i])):
-                y_calculate = self.weight[j] * self.input_matrix[i][j]
+            for j in range(self.size_window):
+                y_calculate += self.y_true[-1 * self.size_window + i + j]*self.weight[j]
 
-            y_calculate_vector.append(y_calculate)
-            for j in range(len(self.input_matrix[i])):
-                self.weight[j] += (y_calculate - self.input_matrix[i + 1][-1]) * self.rate_learning * self.input_matrix[i][j]
+            for j in range(self.size_window):
+                self.weight[j] += (self.y_true[i] - y_calculate) * self.rate_learning * self.y_true[-1 * self.size_window + i + j]
 
-    #def next_calculate(self, x_vector):
-        #return
+        self.epochs += 1
 
+    def neuron_prediction(self, number):
+        y_prediction = self.y_true[-1 * self.size_window:]
+        for i in range(number):
+            next_prediction = 0
+            for j in range(self.size_window):
+                next_prediction += y_prediction[-1 * self.size_window + j]*self.weight[j]
+
+            y_prediction.append(next_prediction)
+
+        return y_prediction[self.size_window:]
+
+    def calculate_errors(self):
+        self.error_on_last_epochs = 0
+        self.vector_ans_on_epoch = []
+        for i in range(self.size_window, len(self.y_true) - 1):
+            y_calculate = 0
+            for j in range(self.size_window):
+                y_calculate += self.y_true[-1 * self.size_window + i + j] * self.weight[j]
+
+            self.vector_ans_on_epoch.append(y_calculate)
+
+        for i in range(len(self.vector_ans_on_epoch)):
+            self.error_on_last_epochs += (self.vector_ans_on_epoch[i] - self.y_true[self.size_window + i]) ** 2
+
+        self.error_on_last_epochs = self.error_on_last_epochs ** (0.5)
+        return self.error_on_last_epochs
--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,42 +1,66 @@
 import math
 from matplotlib import mlab, pyplot
 from Window import Window
+import random
 
-def function(a, b, N, size_window):
+def function(a, b, N):
     step = (b - a) / N
     input_vectors = [a]
-    input_matrix = []
     y_true = []
-    while input_vectors[-1] < b:
-        y_true.append(round(float((math.cos(input_vectors[-1])) ** 2 - 0.5), 3))
-        input_vectors.append(round(float(input_vectors[-1] + step), 3))
+    while input_vectors[-1] < (2 * b - a):
+        y_true.append((math.cos(input_vectors[-1]) ** 2 - 0.5))
+        input_vectors.append((input_vectors[-1] + step))
 
     y_true.append(round(float((math.cos(input_vectors[-1])) ** 2 - 0.5), 3))
-    for i in range(len(y_true) - size_window):
-        input_matrix.append(list((1, y_true[i], y_true[i + 1], y_true[i + 2], y_true[i + 3])))
+    return(input_vectors, y_true)
 
-    return(input_vectors, y_true, input_matrix)
+def find_rate_learning(y_true, size_window):
+    good_rate = 0
+    min_epochs = 3000
+    for i in range(100):
+        rate = random.random()
+        neuron = Window(1, y_true[:20], size_window)
+        error = 1
+        while error > (0.1 ** (10)):
+            neuron.during_epochs()
+            error = neuron.calculate_errors()
+
+        if neuron.epochs < min_epochs:
+            min_epochs = neuron.epochs
+            good_rate = rate
+
+    print(good_rate, min_epochs)
 
 def main():
     a = -1
     b = 0.5
-    N = 19
+    N = 20
     step = (b - a) / N
     size_window = 4
-    input_vectors, y_true, input_matrix = function(a, b, N, size_window - 1)
-    print(len(input_matrix))
-    neuron = Window(1, input_matrix)
-    neuron.during_epochs()
-    #x_probably = 0.5 + step
-    print(neuron.weight)
-    build_map(input_vectors, y_true)
+    input_vectors, y_true = function(a, b, N)
+    neuron = Window(1, y_true[:20], size_window)
+    error = 1
+    while error > (0.1 ** (10)):
+        neuron.during_epochs()
+        error = neuron.calculate_errors()
 
-def build_map(xlist, ylist):
+    x_probality = [input_vectors[20]]
+    while x_probality[-1] < (2 * b - a):
+       x_probality.append(round(float(x_probality[-1] + step), 3))
+
+    print('Epochs=', neuron.epochs)
+    print('W=', neuron.weight)
+    print("Error=", neuron.error_on_last_epochs)
+    y_probality = neuron.neuron_prediction(21)
+    build_map(input_vectors, y_true, x_probality, y_probality)
+    #find_rate_learning(y_true[20:], size_window)
+
+def build_map(xlist, ylist, x_probality, y_probality):
     pyplot.plot(xlist, ylist)
+    pyplot.plot(x_probality, y_probality)
     pyplot.grid(True)
     pyplot.savefig('123.png')
     pyplot.show()
-
 
 if __name__ == '__main__':
     main()
