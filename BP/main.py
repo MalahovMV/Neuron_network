298f26b8eb400935f05a266dad7fee0650ed5355
malahov <misha.malaxow@yandex.ru>
2017-04-02 12:53:39 +0300

    BP

--- BP/main.py
+++ BP/main.py
@@ -2,16 +2,18 @@ from Neuron import neuron
 
 def main(rate_learn):
     rate_learn = rate_learn
+    e = 0.00001
     y_true = 0.3
     input_x = [1, 2]
     network = neuron(rate_learn, y_true, input_x)
     while True:
         network.calculate_values()
         network.calculate_error()
-        if abs(network.result_error) < 0.00001: break
+        print("Epoch= %2d" % network.epoch, "  y=%7F" % network.y_calculate, '  Error=', network.result_error)
+        if abs(network.result_error) < e: break
         network.change_weights()
 
-    print(network.y_calculate, network.epoch)
+    #print(network.y_calculate, network.epoch)
 
 if __name__ == '__main__':
     main(1)
